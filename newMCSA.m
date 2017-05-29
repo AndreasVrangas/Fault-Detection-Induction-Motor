da9e4f6342d6c91197ac6cd5439c245185ff6067
Andreas Vrangas <a.vrangas@gmail.com>
2017-05-29 13:34:33 +0300

    Update newMCSA.m

--- newMCSA.m
+++ newMCSA.m
@@ -1,35 +1,35 @@
-%Anoigma tou arxeiou excel
+%Open excel file
 fn='Times New Roman';fs=10; fontsize = 10;    
 [d,s,r] = xlsread('1235rpm_3brb');
 freq=xlsread('1235rpm_hlthy',2);
 
-%%eisagwgi dedomenwn
+%%Import Data
 Time = d(:,1);
 Vph  = d(:,2);
 Iph  = d(:,3);
 
-%%orismos diakritwn simeiwn tou M/S Fourier
+%%define Fourier transform discrete points
 N=size(d,1);
 
-%%periodos kai sixnotita deigmatolhpsias
+%%Sample period and Frequency
 Ts = mean(diff(Time));
 Fs =1/Ts;
 
-%%ektipwsi sto pedio tou xronou
+%%plot data over time
 figure(1);
 plot(Time,Iph,'g');
 grid on;
 xlabel('Time (sec)');
 ylabel('Current (A)');
 
-%%metasximatismos fourier
+%%Fast Fourier Transform
 FIph = fft(Iph,N)/size(d,1);
 Fv = Fs/2*linspace(0,1,N/2+1);
 Fi = 1:length(Fv);
 
 saveTightFigure('Plot_1.emf');
 
-%ektipwsi Platos revmatos (se Ambere)-Sixnotitas
+%Plot fft (Amber-Frequency)
 figure(2);
 plot(Fv,abs(FIph(Fi)),'r');
 grid on;
@@ -38,7 +38,7 @@ xlabel('Frequency (Hz)');
 ylabel('Current (A)');
 saveTightFigure('Plot_2.emf');
 
-%ektipwsi se db-sixnotita
+%plot fft (dB-Frequency)
 figure(3);
 plot(Fv,10*log(FIph(Fi)),'r','LineWidth',0.3);hold on;
 grid on;
@@ -48,19 +48,19 @@ ylabel('Amplitude (dB)');
 
 for i = 1:8
     
-    % orio gia anixneusi gyrw apo ti sixnotita (Hz)
+    % threshold to detect over central freq (Hz)
     range_threshold = 3;
-    % proseggisi sixnotitas opou entopizetai armoniki
+    %approach frequency where a harmonic is found 
     freq_approx = round(freq(i));
-    % ypologismos olis tis perioxis
+    % calculate the whole area
     start= freq_approx - range_threshold;
     finish = freq_approx + range_threshold;
     freq_all = [start : 1 : finish];
-    % ypologismos olwn twn entasewn gia tis epimerous sixnotites
+    % calculate current amplitude for every individual frequency
     currents = real(10*log(FIph(freq_all+1)));
-    % euresi thesis megistou current
+    % find max current position
     [~, ind] = max(currents);
-    % euresi sixnotitas pou antistoixei sto megisto current
+    % find frequency corresponding to the max current
     ii = freq_all(ind);
     
     x=[ii,ii];
